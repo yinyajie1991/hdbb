2fab91a8634f5467dbe3766fd14c3e75cf1d9600
yinyajie1991 <yinyajie1991@163.com>
2019-08-17 23:19:46 +0800

    收到的粉丝

---
+++ src/main/java/com/example/hdbb/HdbbApplication.java
@@ -0,0 +1,13 @@
+package com.example.hdbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdbbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HdbbApplication.class, args);
+    }
+
+}
